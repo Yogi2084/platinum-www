f7da9530d93c949350d60f2f2cc53fd5942d15e7
Yogi2084 <1si22is406@sit.ac.in>
2025-05-05 12:02:10 +0530

    Add global styles and layout structure: implement theme support and define CSS variables

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,1 +1,30 @@
-import  './globals.css';
+import { Inter } from "next/font/google";
+import { PropsWithChildren } from "react";
+import "./globals.css";
+import { Metadata } from "next";
+import { ThemeProvider } from "next-themes";
+
+const inter = Inter({
+  variable: "--font-inter",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Platinum",
+  description: "Top tech-news everyday!",
+};
+
+const RootLayout = ({ children }: PropsWithChildren) => {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body className={inter.className}>
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+          {children}
+          rootpage
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+};
+
+export default RootLayout;
